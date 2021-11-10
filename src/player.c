8c83e730fd9e23508a80a1565bffad394c16265e
mleblanc <mleblanc@student.42quebec.com>
2021-11-10 14:48:29 -0500

    diagonal movement speed fixed

--- src/player.c
+++ src/player.c
@@ -6,33 +6,35 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 23:55:12 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/11/10 13:23:19 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/11/10 14:46:54 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "player.h"
 #include <math.h>
 
-static void	move_left(t_player *p, float speed)
+static t_vec2	move_left(t_player *p)
 {
 	t_vec2	left;
 
 	left.x = cosf(p->angle - PI_F / 2.0f);
 	left.y = sinf(p->angle - PI_F / 2.0f);
-	p->pos = vec2_add(p->pos, vec2_mul(left, speed));
+	return (left);
 }
 
-static void	move_right(t_player *p, float speed)
+static t_vec2	move_right(t_player *p)
 {
 	t_vec2	right;
 
 	right.x = cosf(p->angle + PI_F / 2.0f);
 	right.y = sinf(p->angle + PI_F / 2.0f);
-	p->pos = vec2_add(p->pos, vec2_mul(right, speed));
+	return (right);
 }
 
 void	update_player(t_player *p, bool keystate[N_KEYS], float speed)
 {
+	t_vec2	move_dir;
+
 	if (keystate[LEFT])
 	{
 		p->angle = wrap_angle(p->angle - 0.1f);
@@ -43,12 +45,15 @@ void	update_player(t_player *p, bool keystate[N_KEYS], float speed)
 		p->angle = wrap_angle(p->angle + 0.1f);
 		p->dir = vec2_rotate(p->dir, p->angle);
 	}
+	move_dir = (t_vec2){0, 0};
 	if (keystate[W])
-		p->pos = vec2_add(p->pos, vec2_mul(p->dir, speed));
+		move_dir = vec2_add(move_dir, p->dir);
 	if (keystate[S])
-		p->pos = vec2_sub(p->pos, vec2_mul(p->dir, speed));
+		move_dir = vec2_sub(move_dir, p->dir);
 	if (keystate[A])
-		move_left(p, speed);
+		move_dir = vec2_add(move_dir, move_left(p));
 	if (keystate[D])
-		move_right(p, speed);
+		move_dir = vec2_add(move_dir, move_right(p));
+	move_dir = vec2_normalize(move_dir);
+	p->pos = vec2_add(p->pos, vec2_mul(move_dir, speed));
 }
