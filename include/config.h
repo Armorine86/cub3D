508ac9a5c3490756b091d68e0a20de7aa6e358b3
mikastiv <m.leblanc_3@hotmail.com>
2021-11-17 02:22:17 -0500

    bigger map

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 19:00:47 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/11/16 23:21:47 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/11/17 02:18:58 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 # define WIDTH 1600
 # define HEIGHT 600
-# define SQ_SIZE 100
+# define SQ_SIZE 50
 # define PAD_SIZE 3
-# define MAP_W 8
-# define MAP_H 6
+# define MAP_W 16
+# define MAP_H 12
 # define MAX_DIST 20.0
 # define SPEED 2.5
 # define A_SPEED 4.0
