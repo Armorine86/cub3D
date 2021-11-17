272d0dd63eb31c1cf7e0d79aea3e3ebb621273ad
mikastiv <m.leblanc_3@hotmail.com>
2021-11-17 02:29:56 -0500

    useless code

--- src/player.c
+++ src/player.c
@@ -6,31 +6,13 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 23:55:12 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/11/17 02:16:33 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/11/17 02:27:44 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "player.h"
 #include "config.h"
 #include <math.h>
-
-static t_vec2	move_left(t_player *p)
-{
-	t_vec2	left;
-
-	left.x = cos(p->angle - deg_to_rad(90.0));
-	left.y = sin(p->angle - deg_to_rad(90.0));
-	return (left);
-}
-
-static t_vec2	move_right(t_player *p)
-{
-	t_vec2	right;
-
-	right.x = cos(p->angle + deg_to_rad(90.0));
-	right.y = sin(p->angle + deg_to_rad(90.0));
-	return (right);
-}
 
 void	update_player(t_player *p, bool keystate[N_KEYS], double dt)
 {
@@ -56,9 +38,9 @@ void	update_player(t_player *p, bool keystate[N_KEYS], double dt)
 	if (keystate[S])
 		move_dir = vec2_sub(move_dir, p->dir);
 	if (keystate[A] && !keystate[D])
-		move_dir = vec2_add(move_dir, move_left(p));
+		move_dir = vec2_add(move_dir, vec2_unit(p->angle - deg_to_rad(90.0)));
 	if (keystate[D] && !keystate[A])
-		move_dir = vec2_add(move_dir, move_right(p));
+		move_dir = vec2_add(move_dir, vec2_unit(p->angle + deg_to_rad(90.0)));
 	move_dir = vec2_normalize(move_dir);
 	p->pos = vec2_add(p->pos, vec2_mul(move_dir, SPEED * dt));
 }
